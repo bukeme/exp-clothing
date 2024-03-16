3a697c29aece27f3ba6929fbc6e7ba0bcaa7fb9d
Ukeme Bassey <basseyukemme@gmail.com>
2024-03-16 13:02:22 +0100

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils.js';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils.js';
+import { onSnapshot } from 'firebase/firestore';
 
 
 class App extends React.Component {
@@ -20,9 +21,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      // this.setState({ currentUser: user });
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        const unsub = onSnapshot(userRef, snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => {console.log(this.state)})
+        })
+      }
+
+      this.setState({currentUser: userAuth});
     })
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,7 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, createUserWithEmailAndPassword  } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,5 +24,60 @@ const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
-export const signInWithGoogle = () => signInWithPopup(auth, provider);
-export const signout = () => signOut(auth);
+export const signInWithGoogle = () => {
+  signInWithPopup(auth, provider)
+  .then(result => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    const user = result.user;
+    console.log({token, user});
+  })
+  .catch(error => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    const email = error.customData.email;
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    console.log({errorCode, errorMessage, email, credential})
+  });
+};
+export const signout = () => {
+  signOut(auth)
+  .then(() => {
+    console.log('Sign Out Successful')
+  })
+  .catch(error => {
+    console.log(error.message);
+  });
+};
+
+const db = getFirestore(app);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const docRef = doc(db, "users", userAuth.uid);
+  const docSnap = await getDoc(docRef);
+  // console.log(docSnap.exists());
+  // console.log(userAuth);
+  if (!docSnap.exists()) {
+    const { displayName, email, uid } = userAuth;
+    const createdAt = new Date();
+    const newDocRef = doc(db, 'users', uid);
+
+    try {
+      await setDoc(newDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+      return newDocRef;
+
+    } catch(error) {
+      console.log('Error occurred creating user', error.message);
+    }
+  }
+  return docRef;
+}
+
+export const createUserWithEP = async (email, password) => await createUserWithEmailAndPassword(auth, email, password);
